e169bf7a4fea2bd661cdb7440d214a80316a07da
Franzi2106 <franziska.muc@gmail.com>
2025-06-28 11:52:32 +0200

    Connect new workflows

--- core/pipeline/subjectdict.py
+++ core/pipeline/subjectdict.py
@@ -10,3 +10,25 @@ def build_subject_dict(subject_id, t1_path, label_map):
         "label_map": label_map,
         # later: add {roi_index: feature_value} here
     }
+
+
+def subjectdict_workflow(config):
+    """Return a workflow that assembles the subject dictionary."""
+    from nipype import Workflow, Node
+    from nipype.interfaces.utility import Function
+
+    wf = Workflow(name="subjectdict_workflow")
+
+    node = Node(
+        Function(
+            input_names=["subject_id", "t1_path", "label_map"],
+            output_names=["subject_dict"],
+            function=build_subject_dict,
+        ),
+        name="build_subject_dict",
+    )
+
+    node.inputs.subject_id = config.get("APP", "subject_id", fallback="")
+
+    wf.add_nodes([node])
+    return wf
